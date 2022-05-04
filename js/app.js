2301564f818b43fe53537cabf673914d2b4cdc14
Sebastian <sebastianvel1425@gmail.com>
2022-05-04 13:08:50 -0500

    agregadas funciones a la logica del proyecto, mostrarResultados y lectura de evento click para los botones

--- js/app.js
+++ js/app.js
@@ -1,78 +1,76 @@
 //// FUNCIONES USADAS PARA EL PROYECTO DE ENCRIPTACION - CHALLENGE ALURA N°1 ////
-function saltarlinea(){
-    document.write("<br><br><br><br><br>");    
+var btnEncriptar = document.querySelector("#btnEncriptar");
+var btnDesencriptar = document.querySelector("#btnDesencriptar");
+var btnCopiar = document.querySelector("#btnCopiar");
+
+btnEncriptar.addEventListener("click",encriptar);
+btnDesencriptar.addEventListener("click", desencriptar);
+btnCopiar.addEventListener("click", copy);
+// Aqui capturamos y le decimos a los botones que funcion deben ejecutar
+//al momento que el usuario haga click
+
+function capturarVerificar(){
+    var txt = document.getElementById("txtEncriptar").value.toLowerCase();
+    if(txt != ""){
+        ocultarContenido();
+        return txt;
+    }
 }
-//Realiza saltos de linea para separar contenido
+//Funcion para verificar que lo escrito por el usuario no sea nulo y ademas
+//evitar las mayusculas
 
-function copiar(){                
-    var textoCopiar = document.getElementById("txtDesencriptar");                
-    textoCopiar.select();
-    textoCopiar.setSelectionRange(0, 99999);
-    document.execCommand('copy');
-}                              
-//Funcion para copiar el contenido resultante de la encriptacion.
+function ocultarContenido(){
+    var txtDesencriptar = document.getElementById("txtDesencriptar");
+    var img = document.getElementById("imgFoto");
+    var p = document.getElementById("label1");
+    var label =document.getElementById("label2");
+    var btnCopiar =document.getElementById("btnCopiar");        
+    img.style.display = "none";
+    p.style.display = "none";
+    label.style.display = "none";
+    btnCopiar.style.display = "block";
+    txtDesencriptar.style.display = "block"
+}
+//Funcion cuyo objetivo es ocultar los elementos que no son necesarios 
+//una vez puesta en marcha la aplicacion y asi mismo mostrar lo que 
+//son necesarios
 
-function capturar(){                
-    var textoEncriptar = document.getElementById("textoEncriptar").value.toLowerCase();                
-    if(textoEncriptar != ""){                                                            
-        ocultarContenido();
-        var frase = encriptar(textoEncriptar);
-        document.getElementById("txtDesencriptar").value = frase; 
-        document.getElementById("textoEncriptar").value = "";
-    }                                    
-}  
-//La funcion captura el valor digitado por el usuario, lo convertimos a minusculas en caso de 
-//que se haya insertado una mayuscula, verificamos que no este vacia la variable,
-//si tiene información ejecutamos la funcion que oculta el contenido no necesario
-//enviamos el texto a encriptar, para luego recibir el resultado y mostrarselo al usuario
-
-function capturarDesencriptar(){                
-    var textoEncriptar = document.getElementById("textoEncriptar").value.toLowerCase();                
-    if(textoEncriptar != ""){                                                            
-        ocultarContenido();
-        var frase = desencriptar(textoEncriptar);
-        document.getElementById("txtDesencriptar").value = frase; 
-        document.getElementById("textoEncriptar").value = "";
-    }                                    
-}  
-//Funcion para desencriptar el contenido, tiene el mismo funcionamiento que la encriptacion
-var text;
-
-
-function encriptar (text){
+function encriptar(){
+    var txt = capturarVerificar();    
     var ltr;
-    var frase = "";
-    for(var i = 0; i<text.length; i++){                    
-        switch (text.charAt(i)) {
+    var frase = "";    
+    for(var i = 0; i<txt.length; i++){                    
+        switch (txt.charAt(i)) {
             case "a":
-                ltr = text.charAt(i).replace("a", "ai");                                                                                    
+                ltr = txt.charAt(i).replace("a", "ai");                                                                                    
                 break;
             case "e":
-                ltr = text.charAt(i).replace('e', 'enter');                             
+                ltr = txt.charAt(i).replace('e', 'enter');                             
                 break;                            
             case "i":
-                ltr = text.charAt(i).replace('i', 'imes'); 
+                ltr = txt.charAt(i).replace('i', 'imes'); 
                 
             break;
             case "o":
-                ltr = text.charAt(i).replace('o', 'ober');                             
+                ltr = txt.charAt(i).replace('o', 'ober');                             
             break;
             case "u":
-                ltr = text.charAt(i).replace('u', 'ufat');                             
+                ltr = txt.charAt(i).replace('u', 'ufat');                             
             break;
             default:
-                ltr = text.charAt(i);                            
+                ltr = txt.charAt(i);                            
                 break;
         }         
         frase = frase + ltr;                
-    }    
-    return frase;
+    }            
+    mostrarResultado(frase);
 }
 
 //Toma la frase a encriptar, la descompone letra a letra buscando y reemplazando las vocales
-//para al final reagrupar la frase ya procesada, al final la retornamos
+//para al final reagrupar la frase ya procesada, al final la mostramos el resultado en pantalla 
 
-function desencriptar(text){    
+function desencriptar(){    
+    var text = capturarVerificar();
     var frase = "";
     frase = text.replace(/enter/igm, "e");
     frase = frase.replace(/ober/igm , "o");
@@ -80,37 +78,24 @@ function desencriptar(text){
     frase = frase.replace(/ai/igm , "a");
     frase = frase.replace(/ufat/igm , "u");
 
-    return frase;
-
-    //var arr = text.split(" ");
-    //console.log(arr);
-    
-    //var frase = " "; 
-    //var cadena = "";
-    
-    /*
-    for(var i = 0; i < arr.length; i++){
-        cadena = arr[i].replace(/enter/g, "e");
-        cadena = cadena.replace(/ober/g , "o");
-        cadena = cadena.replace(/imes/g , "i");
-        cadena = cadena.replace(/ai/g , "a");
-        cadena = cadena.replace(/ufat/g , "u");
-        frase = frase + " " + cadena;
-    }*/
+    mostrarResultado(frase);
 }
+
 //Funcion que toma la frase encriptada y busca por todas las palabras que contengan las palabras encriptadas y las 
 //regresa a su estado original
 
-function ocultarContenido(){
-    var img = document.getElementById("imagen");
-    var label1 = document.getElementById("label1");
-    var label2 = document.getElementById("label2");
-    var txtDesencriptar = document.getElementById("txtDesencriptar");
-    img.style.visibility = "collapse";
-    label1.style.visibility = "collapse";
-    label2.style.visibility = "collapse";
-    txtDesencriptar.style.visibility = "visible";
-    txtDesencriptar.style.marginTop = "";               
+function mostrarResultado(txt){    
+    document.getElementById("txtEncriptar").value = "";
+    document.getElementById("txtDesencriptar").value = txt;
 }
-//Funcion para ocultar el contenido y mostrar el espacio donde se muestra la frase desencriptada 
-//y el boton copiar
+
+//funcion cuyo objetivo es mostrar los resultados obtenidos de las anteriores funciones
+
+function copy(){
+    var textoCopiar = document.getElementById("txtDesencriptar");                
+    textoCopiar.select();
+    textoCopiar.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+}
+
+//Funcion para copiar el contenido resultante.
